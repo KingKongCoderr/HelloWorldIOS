84b5af53b12719f5715db067fd36e23be59e9df3
Tankasala, Nandeesh <Nandeesh.Tankasala@meijer.com>
2019-04-21 19:18:31 -0400

    Added assets and ibaction to a UILabel by attaching GestureListener

--- HelloWorldIOS/IamRichViewController.swift
+++ HelloWorldIOS/IamRichViewController.swift
@@ -10,8 +10,28 @@ import UIKit
 
 class IamRichViewController: UIViewController{
     
+    @IBOutlet weak var richTextField: UIView!
+    @IBOutlet weak var backgroundView: UIView!
+    
+    var isPurple = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //making action for UILabel
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.onRichLabelTap(_:)))
+        richTextField.isUserInteractionEnabled = true
+        richTextField.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func onRichLabelTap(_ sender: UITapGestureRecognizer) {
+        if isPurple{
+            backgroundView.backgroundColor = UIColor.black
+            isPurple = false
+        }else{
+            backgroundView.backgroundColor = UIColor.purple
+            isPurple = true
+        }
     }
 }
