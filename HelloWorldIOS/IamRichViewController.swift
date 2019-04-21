03da4ae1a0847436892324cd47e962ca8c0cc37a
Tankasala, Nandeesh <Nandeesh.Tankasala@meijer.com>
2019-04-21 14:51:40 -0400

    using separate viewcontroller than the EntryViewController for iamRich screen

---
+++ HelloWorldIOS/IamRichViewController.swift
@@ -0,0 +1,17 @@
+//
+//  IamRichViewController.swift
+//  HelloWorldIOS
+//
+//  Created by Tankasala, Nandeesh on 4/21/19.
+//  Copyright © 2019 Tankasala, Nandeesh. All rights reserved.
+//
+
+import UIKit
+
+class IamRichViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
